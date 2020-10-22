9147b21df4fd592318baf71416dcd08192c22664
张春 <2934024192@qq.com>
2020-10-22 19:07:06 +0800

    商品管理

---
+++ ElectricityManagementMVC/ElectricityManagementMVC/Controllers/CommodityController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectricityManagementMVC.Controllers
+{
+    public class CommodityController : Controller
+    {
+        public IActionResult CommShow()
+        {
+            return View();
+        }
+        public IActionResult CommAdd()
+        {
+            return View();
+        }
+        public IActionResult CommAdd2()
+        {
+            return View();
+        }
+        public IActionResult CommDel()
+        {
+            return View();
+        }
+        public IActionResult Specification()
+        {
+            return View();
+        }
+        
+        public IActionResult CommEvaluate()
+        {
+            return View();
+        }
+        public IActionResult Brand()
+        {
+            return View();
+        }
+        public IActionResult BrandAdd()
+        {
+            return View();
+        }
+
+    }
+}
