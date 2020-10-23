ad1d0cf2a13f43cf3e142e23673d8cf68ccb3d79
ttf0512 <1799441665@qq.com>
2020-10-23 09:42:51 +0800

    lllll

---
+++ ElectricityManagementMVC/ElectricityManagementMVC/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectricityManagementMVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult deliver()
+        {
+            return View(); 
+        }
+        public IActionResult orderDetails(string ids)
+        {
+            ViewBag.Is = ids;
+            return View();
+        }
+    }
+}
