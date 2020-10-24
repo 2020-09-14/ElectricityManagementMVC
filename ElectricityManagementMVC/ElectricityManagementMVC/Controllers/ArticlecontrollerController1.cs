85d4fe79577c14fde8662664900e3414964497a6
HP <HP@DESKTOP-HNEDNFK>
2020-10-24 08:15:19 +0800

    lllll

---
+++ ElectricityManagementMVC/ElectricityManagementMVC/Controllers/ArticlecontrollerController1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectricityManagementMVC.Controllers
+{
+    public class ArticlecontrollerController1 : Controller
+    {
+        public IActionResult GetShow()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult classify()
+        {
+            return View();
+        }
+
+        public IActionResult Bang()
+        {
+            return View();
+        }
+
+        public IActionResult Gai()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ElectricityManagementMVC/ElectricityManagementMVC/Controllers/SalesExchangeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectricityManagementMVC.Controllers
+{
+    public class SalesExchangeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TuiShow()
+        {
+            return View();
+        }
+    }
+}
