e7ae9d48521075c141386d168815b481003fca68
KangGeZuiShuai <2534280170@qq.com>
2020-10-29 16:25:52 +0800

    ok

---
+++ ElectricityManagementMVC/ElectricityManagementMVC/Controllers/ActivityController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectricityManagementMVC.Controllers
+{
+    public class ActivityController : Controller
+    {
+        
+        public IActionResult ShowActivities()
+        {
+            return View();
+        }
+
+        public IActionResult ShowProduct()
+        {
+            return View();
+        }
+
+
+        public IActionResult EditActivities(int Id)
+        {
+            ViewBag.Id = Id;
+            return View();
+        }
+
+        public IActionResult Showcoupon()
+        {
+            return View();
+        }
+
+        public IActionResult AddActivities()
+        {
+            return View();
+        }
+
+        public IActionResult Addcoupon()
+        {
+            return View();
+        }
+
+        //public IActionResult Editcoupon()
+        //{
+        //    return View();
+        //}
+
+
+        public IActionResult Showlanding()
+        {
+            return View();
+        }
+
+        public IActionResult Addlanding()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
